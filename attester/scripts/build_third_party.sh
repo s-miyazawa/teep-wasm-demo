fdc3bd0f8a43cc1d3fe2efcf4380a865a51f60cc
yuma-nishi <yuma1114s66@gmail.com>
2025-10-17 09:35:19 +0900

    fix a Makefile and a Dockerfile

---
+++ attester/scripts/build_third_party.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")/../third_party"
+mkdir -p ../build/lib
+
+echo "[INFO] Building QCBOR..."
+make -C QCBOR -B libqcbor.a
+mv QCBOR/libqcbor.a ../build/lib/
+
+echo "[INFO] Building t_cose..."
+make -C t_cose -B -f Makefile.ossl libt_cose.a QCBOR_INC='-I ../QCBOR/inc' 
+mv t_cose/libt_cose.a ../build/lib/
+
+echo "[INFO] Building libcsuit..."
+make -C libcsuit -B -f Makefile libcsuit.a LOCAL_CFLAGS='$(WARNING_CFLAGS) -fPIC -I /usr/local/include -I ./inc -I ../QCBOR/inc -I ../t_cose/inc'
+mv libcsuit/libcsuit.a ../build/lib/
+
+echo "[INFO] Building libteep..."
+make -C libteep -B -f Makefile libteep.a INC='-I./inc -I../QCBOR/inc -I../t_cose/inc'
+mv libteep/libteep.a ../build/lib/
